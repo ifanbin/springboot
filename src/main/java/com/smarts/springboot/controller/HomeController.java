4ce543b36967503c1eab0c73205f0245c54d7a15
ifanbin <ifanbin@outlook.com>
2018-05-28 06:35:04 +0800

    home url 可以访问

--- src/main/java/com/smarts/springboot/controller/HomeController.java
+++ src/main/java/com/smarts/springboot/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.smarts.springboot.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
+@RestController
 public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("hello, springboot");
+        return "hello, springboot";
+    }
 }
