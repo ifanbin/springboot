033a76314e377c7971ae830f969a3589754f6e8b
ifanbin <ifanbin@outlook.com>
2018-05-30 21:13:40 +0800

    增加了controller的测试类

--- src/test/java/com/smarts/springboot/controller/HomeControllerTest.java
+++ src/test/java/com/smarts/springboot/controller/HomeControllerTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class HomeControllerTest {
     @Test
     public void testSayHello() {
+        //用于比较预期和实际是否有差异
         assertEquals("hello world!",new HomeController().sayHello());
     }
 }
