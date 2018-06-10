af423e02a145207e16fbb71943356b692bc2c654
ifanbin <ifanbin@outlook.com>
2018-06-10 11:30:38 +0800

    修改contrlloer的测试类，可以测试网络信息

--- src/test/java/com/smarts/springboot/controller/HomeControllerTest.java
+++ src/test/java/com/smarts/springboot/controller/HomeControllerTest.java
@@ -1,13 +1,37 @@
 package com.smarts.springboot.controller;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.assertEquals;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HomeControllerTest {
-    @Test
-    public void testSayHello() {
-        //用于比较预期和实际是否有差异
-        assertEquals("hello world!",new HomeController().sayHello());
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
     }
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
 }
